fa5c5de3d82d71707e20d9b2f009f801c5fbcd32
Antonio Leonardo Bezerra <abezerra@brasal.com.br>
2019-04-12 04:21:02 -0300

    Add .env.example

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
@@ -7,11 +8,13 @@ require('dotenv').config();
 // Start app
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const { API_PORT, DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
 
 // Start DB
-mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?authSource=admin`, {useNewUrlParser: true});
+// mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?authSource=admin`, {useNewUrlParser: true});
+mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`, {useNewUrlParser: true});
 requireDir('./src/models');
 
 app.use('/api', require('./src/routes'));
